90d8c060bd0114e8082d2c8ff8cf00fdfc25a460
WarllaK <warlla.kelly1@gmail.com>
2023-10-20 21:52:36 -0300

    iniciando servidor

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+import Routes from "./router/Routes";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", Routes);
+
+app.listen(3000, () => {
+  console.log("Server ON! http://localhost:3000/");
+});
